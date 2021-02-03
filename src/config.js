76915096a89046e3c96395123849cd38815aba58
Vladimir Suslov <vladimirsuslov@Vladimirs-MacBook-Pro.local>
2021-02-02 19:38:44 -0500

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "fba1-api-prod-serverlessdeploymentbucket-mjf0vi6o8bee",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://4ollopqnxg.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_ZwUevLYTv",
+      APP_CLIENT_ID: "4go9osv06469m47dlrekgnmbcp",
+      IDENTITY_POOL_ID: "us-east-1:b21d13ba-5c7f-4efc-822a-5a8c1abc2f0e",
+    },
+  };
+  
+  export default config;
